4bb84cc48d75ed4f7b3f70476b8709bcef0b6b1d
Oleksiy Slyshyk <alexslyshyk@gmail.com>
2019-06-30 11:05:27 +0300

    add travis CI

---
+++ .travis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "-- C compilers available"
+ls -1 /usr/bin/gcc*
+ls -1 /usr/bin/clang*
+ls -1 /usr/bin/scan-build*
+echo "----"
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+	sudo apt-get update -qq || true
+else
+	brew install libusb
+fi
+
+echo "=== Building Debug"
+mkdir -p build/Debug && cd build/Debug && cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$PWD/_install ../../ && make && make package && cd -
+
+echo "=== Building Release"
+mkdir -p build/Release && cd build/Release && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/_install ../../ && make && make package && cd -
